dc26ccf39bec997e768bc363069a32f4bb1b563d
gaspar-ion <gaspar@ionsmart.eu>
2023-12-05 10:03:29 +0100

    socket io template

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { join } = require('node:path');
+const { createServer } = require('node:http');
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
@@ -29,9 +31,7 @@ app.use(
 
 // ============ ROUTES ===========
 app.get("/", (req, res) => {
-    res.status(200).send({
-        "message": "Route working"
-    });
+    res.status(200).sendFile(join(__dirname, 'index.html'));
 });
 
 app.post("/say_something", (req, res) => {
@@ -54,10 +54,33 @@ app.post("/ordenar_numeros", (req, res) => {
 });
 
 // ============ LISTEN SERVER ===========
-app.listen(PORT, HOST, () => {
+const httpServer = createServer(app);
+
+httpServer.listen(PORT, HOST, () => {
     console.log("Listening on *:" + PORT);
 });
 
-function sumar(numeros) {
+// ============== SOCKET IO ==============
+const { Server } = require('socket.io');
+const socketIO = new Server(httpServer, {
+    cors: {
+        origin: true,
+        credentials: true
+    }
+});
 
-}
+socketIO.on('connection', (socket) => {
+    console.log(`Un usuario se ha conectado: ${socket.id}`);
+
+    socket.on('click_boton', (data) => {
+        console.log("El usuario ha hecho click al botón", data);
+
+        socket.broadcast.emit('nuevo_mensaje', {
+            texto: data.texto
+        })
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Un usuario se ha desconectado');
+    });
+})
